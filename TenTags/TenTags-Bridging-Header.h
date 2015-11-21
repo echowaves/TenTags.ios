190198d6c556ba6399f5e3da9ba03392eee301ef
dmitryame <dmitry@custommode.com>
2015-11-20 22:06:34 -0500

    configured localytics

---
+++ TenTags/TenTags-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  TenTags-Bridging-Header.h
+//  TenTags
+//
+//  Created by D on 11/20/15.
+//  Copyright © 2015 EchoWaves. All rights reserved.
+//
+
+#ifndef TenTags_Bridging_Header_h
+#define TenTags_Bridging_Header_h
+
+
+#endif /* TenTags_Bridging_Header_h */
