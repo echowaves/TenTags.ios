5295561a7bade1e8d2586c4c32875523d09747d0
dmitryame <dmitry@custommode.com>
2015-11-22 19:52:46 -0500

    perfecting

--- TenTags/ThemViewController.swift
+++ TenTags/ThemViewController.swift
@@ -19,32 +19,32 @@ class ThemViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let location = "1 Infinity Loop, Cupertino, CA"
-        var geocoder:CLGeocoder = CLGeocoder()
-        geocoder.geocodeAddressString(location, completionHandler: {(placemarks, error) -> Void in
-            
-            if((error) != nil){
-                
-                print("Error", error)
-            }
-                
-            else if let placemark = placemarks![0] as? CLPlacemark {
-                
-                var placemark:CLPlacemark = placemarks![0] as! CLPlacemark
-                var coordinates:CLLocationCoordinate2D = placemark.location!.coordinate
-                
-                var pointAnnotation:MKPointAnnotation = MKPointAnnotation()
-                pointAnnotation.coordinate = coordinates
-                pointAnnotation.title = "Apple HQ"
-                
-                self.mapView?.addAnnotation(pointAnnotation)
-                self.mapView?.centerCoordinate = coordinates
-                self.mapView?.selectAnnotation(pointAnnotation, animated: true)
-                
-                print("Added annotation to map view")
-            }
-            
-        })
+//        let location = "1 Infinity Loop, Cupertino, CA"
+//        var geocoder:CLGeocoder = CLGeocoder()
+//        geocoder.geocodeAddressString(location, completionHandler: {(placemarks, error) -> Void in
+//            
+//            if((error) != nil){
+//                
+//                print("Error", error)
+//            }
+//                
+//            else if let placemark = placemarks![0] as? CLPlacemark {
+//                
+//                var placemark:CLPlacemark = placemarks![0] as! CLPlacemark
+//                var coordinates:CLLocationCoordinate2D = placemark.location!.coordinate
+//                
+//                var pointAnnotation:MKPointAnnotation = MKPointAnnotation()
+//                pointAnnotation.coordinate = coordinates
+//                pointAnnotation.title = "Apple HQ"
+//                
+//                self.mapView?.addAnnotation(pointAnnotation)
+//                self.mapView?.centerCoordinate = coordinates
+//                self.mapView?.selectAnnotation(pointAnnotation, animated: true)
+//                
+//                print("Added annotation to map view")
+//            }
+//            
+//        })
         
     }
     
