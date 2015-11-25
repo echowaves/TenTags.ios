6b6f4a20bf4579ff602b53bc437474af790929e1
dmitryame <dmitry@custommode.com>
2015-11-25 11:29:25 -0500

    autocompletion tags search

--- TenTags/AddNewTagViewController.swift
+++ TenTags/AddNewTagViewController.swift
@@ -8,12 +8,15 @@
 
 import UIKit
 
-class AddNewTagViewController: UIViewController, UITextFieldDelegate {
+class AddNewTagViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource, UIPopoverPresentationControllerDelegate {
     
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var addButton: UIButton!
     
+    
+    @IBOutlet weak var autocompleteTableView: UITableView!
+    var completions = [String]()
     
     
     override func viewDidLoad() {
@@ -21,6 +24,20 @@ class AddNewTagViewController: UIViewController, UITextFieldDelegate {
         // Do any additional setup after loading the view, typically from a nib.
         textField.becomeFirstResponder()
         textField.delegate = self
+        
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.addObserver(
+            self,
+            selector: "textFieldTextChanged:",
+            name:UITextFieldTextDidChangeNotification,
+            object: textField
+        )
+
+        
+        autocompleteTableView.hidden = true
+        autocompleteTableView.delegate      =   self
+        autocompleteTableView.dataSource    =   self
+
     }
     
     @IBAction func backButtonPressed(sender: AnyObject) {
@@ -46,5 +63,44 @@ class AddNewTagViewController: UIViewController, UITextFieldDelegate {
         return newString.length <= maxLength
     }
 
+    
+    func textFieldTextChanged(sender : AnyObject) {
+        NSLog("searching for text: " + textField.text!); //the textView parameter is the textView where text was changed
+        TTHashTag.autoComplete(textField.text!, succeeded: { (results) -> () in
+            
+            self.autocompleteTableView.hidden = false
+            self.completions = results
+            self.autocompleteTableView.reloadData()
+            self.autocompleteTableView.reloadInputViews()
+            
+            }) { (error) -> () in
+                NSLog("error autocompleting")
+        }
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.completions.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = autocompleteTableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell!
+        
+        cell.textLabel?.text = self.completions[indexPath.row]
+        cell.textLabel?.textColor = UIColor(rgb: 0xff8000)
+        
+        return cell
+        
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        autocompleteTableView.hidden = true
+        print("You selected cell #\(self.completions[indexPath.row])!")
+        textField.text = self.completions[indexPath.row]
+    }
+    
+
+    
 }
 
--- TenTags/TTHashTag.swift
+++ TenTags/TTHashTag.swift
@@ -32,79 +32,39 @@ class TTHashTag: NSObject {
         }
         
     }
-
     
-//    class func autoComplete(
-//        location: PFGeoPoint,
-//        searchText: String,
-//        succeeded:(results:[String]) -> (),
-//        failed:(error: NSError!) -> ()
-//        ) -> () {
-//            let queryPost = PFQuery(className:PJPOST.CLASS_NAME)
-//            // Interested in locations near user.
-//            queryPost.whereKey(PJPOST.location, nearGeoPoint:location)
-//            queryPost.whereKey(PJPOST.active, equalTo:true)
-//            queryPost.whereKey(PJPOST.archived, equalTo:false)
-//            queryPost.whereKey(PJPOST.createdBy, notEqualTo: PFUser.currentUser()!.objectId!)  //TODO: uncomment
-//            NSLog("Searching for string \(searchText)")
-//            
-//            
-//            let queryTag = PFQuery(className:PJHASHTAG.CLASS_NAME)
-//            queryTag.whereKey(PJHASHTAG.post, matchesQuery:queryPost)
-//            queryTag.whereKey(PJHASHTAG.hashTag, hasPrefix: searchText)
-//            // Limit what could be a lot of points.
-//            queryTag.limit = 100
-//            // Final list of objects
-//            //                self.postsNearMe =
-//            
-//            queryTag.findObjectsInBackgroundWithBlock({ (objects: [AnyObject]?, error: NSError?) -> Void in
-//                if error == nil {
-//                    // The find succeeded.
-//                    // Do something with the found objects
-//                    
-//                    NSLog("results: \(objects!.count)")
-//                    let alltags = (objects as! [PFObject]).map{$0[PJHASHTAG.hashTag]!}
-//                    var hashTags:Set<String> = Set<String>()
-//                    var orderedResults:[String] = [String]()
-//                    for tag in alltags {
-//                        hashTags.insert(tag as! String)
-//                    }
-//                    for tag in hashTags.sort() {
-//                        orderedResults.append(tag)
-//                    }
-//                    
-//                    succeeded(results: orderedResults)
-//                } else {
-//                    // Log details of the failure
-//                    failed(error: error)
-//                }
-//            })
-//    }
+    class func autoComplete(
+        searchText: String,
+        succeeded:(results:[String]) -> (),
+        failed:(error: NSError!) -> ()
+        ) -> () {
+            let queryTag = PFQuery(className:TTHASHTAG.CLASS_NAME)
+            queryTag.whereKey(TTHASHTAG.hashTag, hasPrefix:searchText)
+            // Limit what could be a lot of points.
+            queryTag.limit = 10
+//            queryTag.orderByAscending(TTHASHTAG.hashTag)
+            
+            queryTag.findObjectsInBackgroundWithBlock { (objects:[PFObject]?, error: NSError?) -> Void in
+                if error == nil {
+                    // The find succeeded.
+                    // Do something with the found objects
+                    
+                    NSLog("results: \(objects!.count)")
+                    let alltags = objects?.map{$0[TTHASHTAG.hashTag]!}
+                    var hashTags:Set<String> = Set<String>()
+                    var orderedResults:[String] = [String]()
+                    for tag in alltags! {
+                        hashTags.insert(tag as! String)
+                    }
+                    for tag in hashTags.sort() {
+                        orderedResults.append(tag)
+                    }
+                    succeeded(results: orderedResults)
+                } else {
+                    // Log details of the failure
+                    failed(error: error)
+                }
+            }
+    }
     
-//    class func loadTagsForPost(post:PFObject)-> ([PFObject]) {
-//        let query = PFQuery(className:PJHASHTAG.CLASS_NAME)
-//        query.whereKey(PJHASHTAG.post, equalTo:post)
-//        query.orderByAscending(PJHASHTAG.hashTag)
-//        return query.findObjects() as! [PFObject]
-//    }
-//    
-//    class func loadTagsForPostInBackground(post:PFObject,
-//        succeeded:(hashTags:[PFObject]) -> (),
-//        failed:(error: NSError!) -> ()
-//        ) {
-//            let query = PFQuery(className:PJHASHTAG.CLASS_NAME)
-//            query.whereKey(PJHASHTAG.post, equalTo:post)
-//            query.orderByAscending(PJHASHTAG.hashTag)
-//            query.findObjectsInBackgroundWithBlock({ (objects: [AnyObject]?, error: NSError?) -> Void in
-//                if error == nil {
-//                    succeeded(hashTags: objects as! [PFObject])
-//                } else {
-//                    // Log details of the failure
-//                    failed(error: error)
-//                }
-//            })
-//    }
-
-    
-
 }
